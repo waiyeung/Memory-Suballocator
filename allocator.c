eabd9f78935c2719bc1be023099d01ddbd6586ab
eftae <Wayne@192-168-1-105.tpgi.com.au>
2014-08-21 03:11:38 +1000

    change multiple return and skeleton for sal_free

--- allocator.c
+++ allocator.c
@@ -38,30 +38,32 @@ static vsize_t memory_size;      // number of bytes malloc'd in memory[]
 
 void sal_init(u_int32_t size) {
     // Do nothing if allocator has already been initiated
-    if (memory != NULL) {
-        return;
-    }
-    
-    // Convert size to a power of two if it isn't already (the smallest that is larger than the input size)
-    memory_size = 2;
-    while (memory_size < size) {
-        memory_size *= 2;
-    }    
-
-    free_list_ptr = FREE_PTR_START;
-
-    memory = malloc(memory_size);
     if (memory == NULL) {
-        fprintf(stderr, "sal_init: insufficient memory");
-        abort();
-    }
 
-    // Create the memory header
-    free_header_t *header = (free_header_t *) memory;
-    header->magic = MAGIC_FREE;
-    header->size = memory_size;
-    header->next = FREE_PTR_START;
-    header->prev = FREE_PTR_START;
+        // Convert size to a power of two if it isn't already 
+        // (the smallest that is larger than the input size)
+        memory_size = 2;
+        while (memory_size < size) {
+            memory_size *= 2;
+        }    
+
+        //initialize memory and update gobal vars
+        free_list_ptr = FREE_PTR_START;
+        memory = malloc(memory_size);
+
+        if (memory == NULL) {
+            fprintf(stderr, "sal_init: insufficient memory");
+            abort();
+        }
+
+        // Create the memory header
+        free_header_t *header = (free_header_t *) memory;
+        header->magic = MAGIC_FREE;
+        header->size = memory_size;
+        header->next = FREE_PTR_START;
+        header->prev = FREE_PTR_START;
+
+    }
 }
 
 void *sal_malloc(u_int32_t n) {
@@ -71,7 +73,7 @@ void *sal_malloc(u_int32_t n) {
     }
 
     // search for appropriate minimum free region
-    vsize_t N = n + HEADER_SIZE;
+    vsize_t N = n + HEADER_SIZE;    //actual size required include header
     free_header_t *target = NULL;
     free_header_t *startpoint = (free_header_t *) (memory + free_list_ptr);
     free_header_t *curr = startpoint;
@@ -99,51 +101,95 @@ void *sal_malloc(u_int32_t n) {
         curr = (free_header_t *)(memory + curr->next);
     }
 
-    // return NULL if there is not enough space
-    if (target == NULL) {
-        return NULL;
-    }
+
+    // continue iff there is an appropriate space
+    void *returnValue = NULL;
+
+    if (target != NULL) {
      
-    //spilt target to appropriate size
-    vlink_t targetLink = (vlink_t)((void *)target - free_list_ptr);
-    free_header_t *split;
-    while (target->size >= N * 2) {
-        target->size /= 2;
-        split = (free_header_t *)((byte *)target + target->size);
-        split->magic = MAGIC_FREE;
-        split->size = target->size;
-        split->next = target->next;
-        split->prev = targetLink;
-        target->next = targetLink + target->size;
-        if (target->prev == targetLink) {
-            target->prev = target->next;
+        // spilt target to appropriate size
+        vlink_t targetLink = (vlink_t)((byte *)target - (byte *)memory);
+        free_header_t *split;
+
+        while (target->size >= N * 2) {
+            target->size /= 2;
+            split = (free_header_t *)((byte *)target + target->size);
+            split->magic = MAGIC_FREE;
+            split->size = target->size;
+            split->next = target->next;
+            split->prev = targetLink;
+            target->next = targetLink + target->size;
+            // if there is one free region left before splitting
+            // link targer_.prev to the new split region
+            if (target->prev == targetLink) {
+                target->prev = target->next;
+            }
         }
-    }
+        
 
-    // If there is only one free region left then return null
-    if (target->next == targetLink) {
-        return NULL;
-    }
+        // continue if there are more then 1 free regions after splitting
+        if (target->next != targetLink) {
      
-    target->magic = MAGIC_ALLOC;
+            target->magic = MAGIC_ALLOC;
 
-    //curr = the region just before target
-    curr = (free_header_t *)(memory + target->prev);
-    curr->next = target->next;
+            //curr = the region just before target
+            curr = (free_header_t *)(memory + target->prev);
+            curr->next = target->next;
 
-    //curr = the region just after target
-    curr = (free_header_t *)(memory + target->next);  
-    curr->prev = target->prev;
+            //curr = the region just after target
+            curr = (free_header_t *)(memory + target->next);  
+            curr->prev = target->prev;
 
-    if (curr == startpoint) {
-        free_list_ptr = target->next;
-    }
+            if (curr == startpoint) {
+                free_list_ptr = target->next;
+            }
 
-    return ((void *) (target + HEADER_SIZE));    
+            returnValue = (void *) ((byte *)target + HEADER_SIZE); 
+        }
+    } 
+
+    return returnValue;  
 }
 
 void sal_free(void *object) {
-    // TODO
+    //create a pointer to the region
+    free_header_t *obj_ptr = (free_header_t *)(object - HEADER_SIZE);
+
+    //check magic number
+    if (obj_ptr->magic != MAGIC_ALLOC)
+    {
+        fprintf(stderr, "Attempt to free non-allocated memory");
+        abort();
+    }
+
+    //search regions to be inserted in between
+    free_header_t *prev = NULL;
+    free_header_t *curr = (free_header_t *)(memory + free_list_ptr);
+
+    while(curr < obj_ptr && prev < curr) {
+        prev = curr;
+        curr = (free_header_t *)(memory + curr->next);
+    }
+
+    if (prev == NULL){
+        //insert at first position
+
+    } else if (prev > curr){
+        //insert at last position
+
+    } else {
+        //insert at between prev & curr
+
+    }
+    obj_ptr->magic = MAGIC_FREE;
+
+    //call sal_merge()
+}
+
+void sal_merge(void *id) {
+    //Merging
+    //(curr's index/curr->size) odd:bottom even:top
+
 }
 
 void sal_end(void) {
@@ -158,7 +204,7 @@ void sal_stats(void) {
     printf("sal_stats\n");
     // we "use" the global variables here
     // just to keep the compiler quiet
-    memory = memory;
-    free_list_ptr = free_list_ptr;
-    memory_size = memory_size;
+    //memory = memory;
+    //free_list_ptr = free_list_ptr;
+    //memory_size = memory_size;
 }
