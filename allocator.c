8e5d4584573efc1bf26b34f24890347a00a4e3b2
Kelvin Xu <fetchkelvinx@gmail.com>
2014-08-20 19:58:46 +1000

    Fix formatting

--- allocator.c
+++ allocator.c
@@ -12,9 +12,9 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define HEADER_SIZE    sizeof(struct free_list_header)  
-#define MAGIC_FREE     0xDEADBEEF
-#define MAGIC_ALLOC    0xBEEFDEAD
+#define HEADER_SIZE     sizeof(struct free_list_header)  
+#define MAGIC_FREE      0xDEADBEEF
+#define MAGIC_ALLOC     0xBEEFDEAD
 
 typedef unsigned char byte;
 typedef u_int32_t vlink_t;
@@ -22,89 +22,80 @@ typedef u_int32_t vsize_t;
 typedef u_int32_t vaddr_t;
 
 typedef struct free_list_header {
-   u_int32_t magic;           // ought to contain MAGIC_FREE
-   vsize_t size;              // # bytes in this block (including header)
-   vlink_t next;              // memory[] index of next free block
-   vlink_t prev;              // memory[] index of previous free block
+    u_int32_t magic;              // ought to contain MAGIC_FREE
+    vsize_t size;                  // # bytes in this block (including header)
+    vlink_t next;                  // memory[] index of next free block
+    vlink_t prev;                  // memory[] index of previous free block
 } free_header_t;
 
 // Global data
 
-static byte *memory = NULL;   // pointer to start of suballocator memory
+static byte *memory = NULL;    // pointer to start of suballocator memory
 static vaddr_t free_list_ptr; // index in memory[] of first block in free list
-static vsize_t memory_size;   // number of bytes malloc'd in memory[]
+static vsize_t memory_size;    // number of bytes malloc'd in memory[]
 
 
-void sal_init(u_int32_t size)
-{
-   if(memory != NULL) abort();
-   if(size <= HEADER_SIZE) abort();
+void sal_init(u_int32_t size) {
+    if(memory != NULL) abort();
+    if(size <= HEADER_SIZE) abort();
 
-   memory_size = 2;
-   while (memory_size < size) memory_size *= 2;
+    memory_size = 2;
+    while (memory_size < size) memory_size *= 2;
 
-   free_list_ptr = 0;
+    free_list_ptr = 0;
 
-   memory = malloc(memory_size);
-   if (memory == NULL)
-   {
-      fprintf(stderr, "sal_init: insufficient memory");
-      abort();
-   }
+    memory = malloc(memory_size);
+    if (memory == NULL) {
+        fprintf(stderr, "sal_init: insufficient memory");
+        abort();
+    }
 
-   free_header_t * header = (free_header_t*)memory;
-   header->magic = MAGIC_FREE;
-   header->size = memory_size;
-   header->next = 0;
-   header->prev = 0;
+    free_header_t * header = (free_header_t*)memory;
+    header->magic = MAGIC_FREE;
+    header->size = memory_size;
+    header->next = 0;
+    header->prev = 0;
 }
 
-void *sal_malloc(u_int32_t n)
-{
-   if(n <= 4) abort();
+void *sal_malloc(u_int32_t n) {
+    if(n <= 4) abort();
 
-//search for appropriate minimum free region
-   vsize_t N = n + HEADER_SIZE;
-   free_header_t * target = NULL;
-   free_header_t * startpoint = (free_header_t*)(memory + free_list_ptr);
-   free_header_t * curr = startpoint;
+    //search for appropriate minimum free region
+    vsize_t N = n + HEADER_SIZE;
+    free_header_t * target = NULL;
+    free_header_t * startpoint = (free_header_t*)(memory + free_list_ptr);
+    free_header_t * curr = startpoint;
 
-   if (curr->magic != MAGIC_FREE)
-   {
-         fprintf(stderr, "Memory corruption");
-         abort();
-   }
+    if (curr->magic != MAGIC_FREE) {
+        fprintf(stderr, "Memory corruption");
+        abort();
+    }
 
-	if (curr->size >= N)
-   {
-   		target = curr;
-   }
+     if (curr->size >= N) {
+              target = curr;
+    }
 
-	curr = (free_header_t*)(memory + curr->next);
-	while(curr != startpoint)
-	{
-		if (curr->magic != MAGIC_FREE)
-   	    {
-   		fprintf(stderr, "Memory corruption");
-			abort();
-   	    }
+     curr = (free_header_t*)(memory + curr->next);
+     while(curr != startpoint) {
+         if (curr->magic != MAGIC_FREE) {
+              fprintf(stderr, "Memory corruption");
+              abort();
+         }
 
-   	    if (curr->size < target->size && curr->size >= N)
-   	    {
-   	    	target = curr;
-   	    }
+         if (curr->size < target->size && curr->size >= N) {
+                    target = curr;
+         }
 
-   	    curr = (free_header_t*)(memory + curr->next);
-	}
+         curr = (free_header_t*)(memory + curr->next);
+     }
 
-//return NULL if no sufficiency space
-	if (target == NULL) return NULL;   
-	
-//spilt target to appropriate size
+    //return NULL if no sufficiency space
+    if (target == NULL) return NULL;    
+     
+    //spilt target to appropriate size
     vlink_t targetLink = (vlink_t)((void*)target - free_list_ptr);
     free_header_t * split;
-    while(target->size >= N * 2)
-    {
+    while(target->size >= N * 2) {
         target->size /= 2;
         split = (free_header_t*)((byte*)target + target->size);
         split->magic = MAGIC_FREE;
@@ -113,11 +104,11 @@ void *sal_malloc(u_int32_t n)
         split->prev = targetLink;
         target->next = targetLink + target->size;
         if(target->prev == targetLink) target->prev = target->next;
-    }
+     }
 
-//allocate target, return result
+    //allocate target, return result
     if (target->next == targetLink) return NULL;
-    
+     
     target->magic = MAGIC_ALLOC;
 
     //curr = the region just before target
@@ -130,29 +121,26 @@ void *sal_malloc(u_int32_t n)
 
     if(curr == startpoint) free_list_ptr = target->next;
 
-    return ((void*)(target + HEADER_SIZE));   
+    return ((void*)(target + HEADER_SIZE));    
 }
 
-void sal_free(void *object)
-{
-   // TODO
+void sal_free(void *object) {
+    // TODO
 }
 
-void sal_end(void)
-{
-   free(memory);
-   memory = NULL;
-   free_list_ptr = 0;
-   memory_size = 0;
+void sal_end(void) {
+    free(memory);
+    memory = NULL;
+    free_list_ptr = 0;
+    memory_size = 0;
 }
 
-void sal_stats(void)
-{
-   // Optional, but useful
-   printf("sal_stats\n");
+void sal_stats(void) {
+    // Optional, but useful
+    printf("sal_stats\n");
     // we "use" the global variables here
     // just to keep the compiler quiet
-   memory = memory;
-   free_list_ptr = free_list_ptr;
-   memory_size = memory_size;
+    memory = memory;
+    free_list_ptr = free_list_ptr;
+    memory_size = memory_size;
 }
