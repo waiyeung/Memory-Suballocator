38693ea38b4a8e1296e90f2864fb716c97468f19
Kelvin Xu <fetchkelvinx@gmail.com>
2014-08-20 20:07:40 +1000

    Fix formatting again

--- allocator.c
+++ allocator.c
@@ -12,9 +12,9 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define HEADER_SIZE     sizeof(struct free_list_header)  
-#define MAGIC_FREE      0xDEADBEEF
-#define MAGIC_ALLOC     0xBEEFDEAD
+#define HEADER_SIZE sizeof(struct free_list_header)  
+#define MAGIC_FREE  0xDEADBEEF
+#define MAGIC_ALLOC 0xBEEFDEAD
 
 typedef unsigned char byte;
 typedef u_int32_t vlink_t;
@@ -22,17 +22,16 @@ typedef u_int32_t vsize_t;
 typedef u_int32_t vaddr_t;
 
 typedef struct free_list_header {
-    u_int32_t magic;              // ought to contain MAGIC_FREE
-    vsize_t size;                  // # bytes in this block (including header)
-    vlink_t next;                  // memory[] index of next free block
-    vlink_t prev;                  // memory[] index of previous free block
+    u_int32_t magic;    // ought to contain MAGIC_FREE
+    vsize_t size;       // # bytes in this block (including header)
+    vlink_t next;       // memory[] index of next free block
+    vlink_t prev;       // memory[] index of previous free block
 } free_header_t;
 
 // Global data
-
-static byte *memory = NULL;    // pointer to start of suballocator memory
-static vaddr_t free_list_ptr; // index in memory[] of first block in free list
-static vsize_t memory_size;    // number of bytes malloc'd in memory[]
+static byte *memory = NULL;      // pointer to start of suballocator memory
+static vaddr_t free_list_ptr;    // index in memory[] of first block in free list
+static vsize_t memory_size;      // number of bytes malloc'd in memory[]
 
 
 void sal_init(u_int32_t size) {
