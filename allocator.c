872b5845a858ede62316f8b54cfa347fb2d1115e
kelvinx <fetchkelvinx@gmail.com>
2014-08-24 00:22:13 +1000

    Change back to u_int32_t

--- allocator.c
+++ allocator.c
@@ -23,13 +23,13 @@
 #define AFTER 1
 
 typedef unsigned char byte;
-typedef uint32_t vlink_t;
-typedef uint32_t vsize_t;
-typedef uint32_t vaddr_t;
+typedef u_int32_t vlink_t;
+typedef u_int32_t vsize_t;
+typedef u_int32_t vaddr_t;
 typedef int boolean;
 
 typedef struct free_list_header {
-    uint32_t magic;    // ought to contain MAGIC_FREE
+    u_int32_t magic;    // ought to contain MAGIC_FREE
     vsize_t size;       // # bytes in this block (including header)
     vlink_t next;       // memory[] index of next free block
     vlink_t prev;       // memory[] index of previous free block
@@ -43,7 +43,7 @@ static byte *memory = NULL;      // pointer to start of suballocator memory
 static vaddr_t free_list_ptr;    // index in memory[] of first block in free list
 static vsize_t memory_size;      // number of bytes malloc'd in memory[]
 
-void sal_init(uint32_t size) {
+void sal_init(u_int32_t size) {
     // Do nothing if allocator has already been initiated
     if (memory == NULL) {
 
@@ -73,7 +73,7 @@ void sal_init(uint32_t size) {
     }
 }
 
-void *sal_malloc(uint32_t n) {
+void *sal_malloc(u_int32_t n) {
     // The size of every region must be a power of two and greater than 4 bytes
     if (n <= MIN_REGION_SIZE) {
         abort();
